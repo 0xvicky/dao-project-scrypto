0a0280ca40ad7a4bc307fa6d6bfca9ed7773ca92
vicky <tyagivivek528@gmail.com>
2023-08-10 19:42:09 +0530

    Created Dao Token, treasury, Admin, Members, Functions to join, proposals started

---
+++ readme.md
@@ -0,0 +1,1 @@
+APP PACKG: package_sim1ph4gj37ssqs3mw3qkvxu5gax9kqf6aq3rx493f35aee9atkj3ylu8c
--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,78 @@
 use scrypto::prelude::*;
 
 #[blueprint]
-mod hello {
-    struct Hello {
+mod dao {
+
+    struct Dao {
         // Define what resources and data will be managed by Hello components
-        sample_vault: Vault,
+        admin_badge_addr: ResourceAddress,
+        dao_token: Vault,
+        treasury: Vault,
+        entry_fee: Decimal,
+        total_members: Decimal,
     }
 
-    impl Hello {
+    impl Dao {
         // Implement the functions and methods which will manage those resources and data
-
         // This is a function, and can be called directly on the blueprint once deployed
-        pub fn instantiate_hello() -> Global<Hello> {
-            // Create a new token called "HelloToken," with a fixed supply of 1000, and put that supply into a bucket
-            let my_bucket: Bucket = ResourceBuilder::new_fungible(OwnerRole::None)
-                .divisibility(DIVISIBILITY_MAXIMUM)
-                .metadata(metadata! {
+        pub fn new(entry_fee: Decimal) -> (Global<Dao>, Bucket) {
+            //Function to create the authorities : SEller
+            pub fn instantiate_authority(name: &str, symbol: &str) -> Bucket {
+                let badge: Bucket = ResourceBuilder::new_fungible(OwnerRole::None)
+                    .metadata(metadata!(
+                      init{
+                        "name"=>name,locked;
+                        "symbol"=>symbol,locked;
+                        "description"=>"One who can withdraw XRD and can entry_fee",locked;
+                      }
+                    ))
+                    .divisibility(DIVISIBILITY_NONE)
+                    .mint_initial_supply(1);
+
+                badge
+            }
+
+            let dao_bucket: Bucket = ResourceBuilder::new_fungible(OwnerRole::None)
+                .metadata(metadata!(
                     init {
-                        "name" => "HelloToken", locked;
-                        "symbol" => "HT", locked;
+                        "name" => "DAO Token", locked;
+                        "symbol" => "R-DAO", locked;
                     }
-                })
-                .mint_initial_supply(1000);
+                ))
+                .mint_initial_supply(0);
+
+            let admin_badge = instantiate_authority("DAO Admin", "ADMIN");
 
             // Instantiate a Hello component, populating its vault with our supply of 1000 HelloToken
-            Self {
-                sample_vault: Vault::with_bucket(my_bucket),
+            let component = Self {
+                treasury: Vault::new(RADIX_TOKEN),
+                dao_token: Vault::with_bucket(dao_bucket),
+                admin_badge_addr: admin_badge.resource_address(), //To instantiate radix token
+                entry_fee,
+                total_members: Decimal::ZERO,
             }
             .instantiate()
             .prepare_to_globalize(OwnerRole::None)
-            .globalize()
+            .globalize();
+
+            (component, admin_badge)
         }
 
         // This is a method, because it needs a reference to self.  Methods can only be called on components
-        pub fn free_token(&mut self) -> Bucket {
-            info!(
-                "My balance is: {} HelloToken. Now giving away a token!",
-                self.sample_vault.amount()
-            );
-            // If the semi-colon is omitted on the last line, the last value seen is automatically returned
-            // In this case, a bucket containing 1 HelloToken is returned
-            self.sample_vault.take(1)
+        pub fn get_members(&mut self) -> Decimal {
+            info!("Total Members in DAO: {}", self.total_members);
+            self.total_members
         }
+
+        pub fn join(&mut self) {}
+
+        // pub fn withdraw(&mut self, amount: Decimal) -> Bucket {
+        //     info!("Withdrawing Funds...");
+        //     self.treasury.take(amount)
+        // }
+
+        // pub fn change_price(&mut self, new_price: Decimal) {
+        //     self.entry_fee = new_price
+        // }
     }
 }
